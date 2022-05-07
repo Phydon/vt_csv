bded8c9e8375834f299e85485e204d9f2a5c1d2f
Phydon <leann.phydon@gmail.com>
2022-05-07 21:02:10 +0200

    first stuff written

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use std::{io, fs};
+use std::io::Write;
+
+const FILEPATH: &str = "./output.txt";
+
 fn main() {
-    println!("Hello, world!");
+    let x = input();
+    let vt = equation(x);
+
+    write_to_file(&vt.to_string()).expect("Failed to write to file");
+
+    println!("vt = {}", vt);
 }
+
+fn equation(num: f64) -> f64 {
+    // change to whatever you want ot calculate
+    let vt = num + 2.0;
+    vt
+}
+
+fn input() -> f64 {
+    println!("Please enter a number: ");
+
+    let mut inp = String::new();
+    io::stdin().read_line(&mut inp).expect("Failed to read input");
+    let numeric = inp.trim().parse::<f64>();
+
+    match numeric {
+        Ok(_ok) => numeric.unwrap(),
+        Err(_e) => 1 as f64,
+    }
+}
+
+fn write_to_file(content: &String) -> io::Result<()> {
+    let mut file = fs::OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open(FILEPATH)?;
+
+    writeln!(file, "{}", content)?;
+    Ok(())
+    }
